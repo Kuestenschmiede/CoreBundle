9a5ca6db8b40494cbf256d0ec06355838196103e
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2017-08-24 14:17:49 +0200

    replace copyright

--- Resources/contao/config/autoload.php
+++ Resources/contao/config/autoload.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * con4gis
- * @version   2.0.0
+ * @version   php 7
  * @package   con4gis
  * @author    con4gis authors (see "authors.txt")
- * @copyright Küstenschmiede GmbH Software & Design 2016 - 2017.
+ * @copyright Küstenschmiede GmbH Software & Design 2017
  * @link      https://www.kuestenschmiede.de
  */
 /**
