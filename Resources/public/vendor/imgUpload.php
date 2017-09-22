bff2f5a68843a42004b87ca887f4d2845c5775c9
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2017-09-22 16:45:40 +0200

    fix imgUpload

--- Resources/public/vendor/imgUpload.php
+++ Resources/public/vendor/imgUpload.php
@@ -6,7 +6,7 @@ ini_set("display_errors","1");
         define("TL_SCRIPT","SOMETHING");
 //        $sRootPath = dirname($_SERVER['SCRIPT_FILENAME']) . "/../../../../";
 //        require_once($sRootPath . "system/initialize.php");
-        $initialize = $_SERVER["DOCUMENT_ROOT"] . '../system/initialize.php';
+        $initialize = $_SERVER["DOCUMENT_ROOT"] . '/../system/initialize.php';
         if (!file_exists($initialize)) {
             $initialize = '../../../system/initialize.php';
         }
