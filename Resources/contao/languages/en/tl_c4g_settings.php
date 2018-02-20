62e60f7e06e1aa79fa0e10abb965a280a910e649
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2018-02-20 17:25:23 +0100

    translate be settings form

---
+++ Resources/contao/languages/en/tl_c4g_settings.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * con4gis for Contao Open Source CMS
+ *
+ * @version   php 7
+ * @package   con4gis-Core (CoreBundle)
+ * @author    con4gis contributors
+ * @license   GNU/LGPL http://opensource.org/licenses/lgpl-3.0.html
+ * @copyright Küstenschmiede GmbH Software & Design 2015 - 2018
+ * @link      https://www.kuestenschmiede.de
+ */
+
+/** FIELDS **/
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_uitheme_css_select'] = array(
+    'jQuery UI ThemeRoller CSS theme',
+    'Select a standart UI-Theme.'
+);
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_appearance_themeroller_css'] = array(
+    'jQuery UI ThemeRoller CSS file',
+    'Optionally: select the CSS file you created with the jQuery UI ThemeRoller.'
+);
+
+/** INFO **/
+$GLOBALS['TL_LANG']['tl_c4g_settings']['infotext'] =
+    'This settings will used by several con4gis bricks.';
+
+/** LEGENDS **/
+$GLOBALS['TL_LANG']['tl_c4g_settings']['layout_legend'] = "Layout settings";
+
+/** Field References */
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_references']['base']      = 'base';
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_references']['black-tie'] = 'black-tie';
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_references']['blitzer']   = 'blitzer';
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_references']['cupertino'] = 'cupertino';
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_references']['dark-hive'] = 'dark-hive';
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_references']['dot-luv']   = 'dot-luv';
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_references']['eggplant']  = 'eggplant';
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_references']['excite-bike']   = 'excite-bike';
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_references']['flick']         = 'flick';
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_references']['hot-sneaks']    = 'hot-sneaks';
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_references']['humanity']      = 'humanity';
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_references']['le-frog']       = 'le-frog';
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_references']['mint-choc']     = 'mint-choc';
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_references']['overcast']      = 'overcast';
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_references']['pepper-grinder'] = 'pepper-grinder';
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_references']['redmond']       = 'redmond';
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_references']['smoothness']    = 'smoothness';
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_references']['south-street']  = 'south-street';
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_references']['start']         = 'start';
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_references']['sunny']         = 'sunny';
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_references']['swanky-purse']  = 'swanky-purse';
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_references']['trontastic']    = 'trontastic';
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_references']['ui-darkness']   = 'ui-darkness';
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_references']['ui-lightness']  = 'ui-lightness';
+$GLOBALS['TL_LANG']['tl_c4g_settings']['c4g_references']['vader']         = 'vader';
+
+/** OPERATIONS **/
+$GLOBALS['TL_LANG']['tl_c4g_settings']['new'] = array("add settings","");
+$GLOBALS['TL_LANG']['tl_c4g_settings']['edit'] = array("edit settings","");
+$GLOBALS['TL_LANG']['tl_c4g_settings']['copy'] = array("copy settings","");
+$GLOBALS['TL_LANG']['tl_c4g_settings']['delete'] = array("delete settings","");
+$GLOBALS['TL_LANG']['tl_c4g_settings']['show'] = array("show settings","");
