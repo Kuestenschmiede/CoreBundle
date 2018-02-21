6d4a7015be00183fde0c9671d3b1e13cda16288c
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2018-02-21 16:13:06 +0100

    fixed denglish

--- Resources/contao/languages/de/modules.php
+++ Resources/contao/languages/de/modules.php
@@ -16,7 +16,7 @@
 $GLOBALS['TL_LANG']['MOD']['con4gis'] = array('con4gis '.$GLOBALS['con4gis']['version'].' - Baukasten', 'Geoinformationssysteme für Contao');
 $GLOBALS['TL_LANG']['MOD']['con4gis_bricks'] = array('con4gis '.$GLOBALS['con4gis']['version'].' - Bausteine', 'con4gis Erweiterungen');
 
-$GLOBALS['TL_LANG']['MOD']['c4g_core'] = array('Info & Help', 'con4gis Infoseite' );
+$GLOBALS['TL_LANG']['MOD']['c4g_core'] = array('Info & Hilfe', 'con4gis Infoseite' );
 $GLOBALS['TL_LANG']['MOD']['c4g_settings'] = array('Einstellungen', 'Einstellungen für alle Bausteine.');
 
 /**
