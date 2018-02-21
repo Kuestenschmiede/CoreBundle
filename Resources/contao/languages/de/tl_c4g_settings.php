f89a148278d533486dc5afaa392e6175fec5c90f
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2018-02-21 17:37:34 +0100

    copyright 2018

--- Resources/contao/languages/de/tl_c4g_settings.php
+++ Resources/contao/languages/de/tl_c4g_settings.php
@@ -6,7 +6,7 @@
  * @package   con4gis-Core (CoreBundle)
  * @author    con4gis contributors
  * @license   GNU/LGPL http://opensource.org/licenses/lgpl-3.0.html
- * @copyright Küstenschmiede GmbH Software & Design 2015 - 2018
+ * @copyright Küstenschmiede GmbH Software & Design 2011 - 2018 - 2018
  * @link      https://www.kuestenschmiede.de
  */
 
