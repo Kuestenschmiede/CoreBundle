8f6c7dc7078b912697f9feb1e23a1bd45ed60e45
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2018-03-16 14:52:57 +0100

    return to bundle ui version

--- Resources/contao/classes/C4GJQueryGUI.php
+++ Resources/contao/classes/C4GJQueryGUI.php
@@ -52,7 +52,7 @@ class C4GJQueryGUI
 		if ($addJQueryUI || $useTree || $useMaps)
    		{
 			// Include JQuery UI JS - only js to append the css files to the end of $GLOBALS['TL_CSS'] array.
-			$GLOBALS['TL_JAVASCRIPT']['c4g_jquery_ui'] 		= 'assets/jquery-ui/js/jquery-ui.min.js';
+			$GLOBALS['TL_JAVASCRIPT']['c4g_jquery_ui'] 		= 'bundles/con4giscore/vendor/jQuery/jquery-ui-1.12.1.custom/jquery-ui.min.js';
             $GLOBALS['TL_JAVASCRIPT']['c4g_jquery_ui_i18n'] = 'bundles/con4giscore/vendor/jQuery/jquery-ui-i18n.min.js';
             $GLOBALS ['TL_JAVASCRIPT']['c4g_a'] 	        = 'bundles/con4giscore/vendor/jQuery/plugins/jquery.legacy.min.js';
 
