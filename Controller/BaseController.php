e20eb9f98c90fc8a65fcb9a02ac6222a0a1cf6f0
coastforge-cro <claudio.ross@kuestenschmiede.de>
2018-05-09 09:16:22 +0200

    Add base class for controllers

---
+++ Controller/BaseController.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * con4gis - the gis-kit
+ *
+ * @version   php 5
+ * @package   con4gis
+ * @author    con4gis contributors (see "authors.txt")
+ * @license   GNU/LGPL http://opensource.org/licenses/lgpl-3.0.html
+ * @copyright Küstenschmiede GmbH Software & Design 2011 - 2016.
+ * @link      https://www.kuestenschmiede.de
+ */
+
+namespace con4gis\CoreBundle\Controller;
+
+use con4gis\CoreBundle\Resources\contao\classes\C4GApiCache;
+use Contao\Database;
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class BaseController extends Controller
+{
+    protected static $useCache = false;
+
+    protected static $outputFromCache = false;
+
+    protected $responseData = "";
+
+    /**
+     * @var EntityManager
+     */
+    protected $entityManager = null;
+
+    protected function initialize()
+    {
+        $this->entityManager = $this->container->get('doctrine.orm.entity_manager');
+    }
+
+    protected function getCacheRequest(Request $request)
+    {
+        return $request->getRequestUri();
+    }
+
+    protected function getCacheFragments(Request $request)
+    {
+
+        return ['request' => $request->query->all()];
+    }
+
+    protected function checkForCacheSettings($configParam)
+    {
+
+        $this->container->get('contao.framework')->initialize();
+        $cacheSettings = Database::getInstance()->execute("SELECT * FROM tl_c4g_settings LIMIT 1")->fetchAllAssoc();
+        $cacheSettings = $cacheSettings[0]['caching'];
+        self::$useCache = (is_array(deserialize($cacheSettings)) && in_array($configParam, deserialize($cacheSettings)));
+
+    }
+
+    protected function checkAndStoreCachedData(Request $request)
+    {
+        if (($returnData = C4GApiCache::getCacheData($this->getCacheRequest($request), $this->getCacheFragments($request)))  !== false)
+        {
+            self::$outputFromCache = true;
+            $this->responseData = $returnData;
+        }
+    }
+
+    protected function storeDataInCache(Request $request)
+    {
+        C4GApiCache::putCacheData($this->getCacheRequest($request), $this->getCacheFragments($request), $this->responseData);
+    }
+}
