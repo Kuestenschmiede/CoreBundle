b5273d7334a576c8c541fc7aa81b335d85b1228e
coastforge-rro <raphael.ross@kuestenschmiede.de>
2018-11-26 12:00:35 +0100

    add C4GSettingsModel::findSettings()

--- Resources/contao/models/C4gSettingsModel.php
+++ Resources/contao/models/C4gSettingsModel.php
@@ -14,6 +14,8 @@
 
 namespace con4gis\CoreBundle\Resources\contao\models;
 
+use Contao\Database;
+
 /**
  * Class C4gSettingsModel
  * @package con4gis\CoreBundle\Resources\contao\models
@@ -25,4 +27,10 @@ class C4gSettingsModel extends \Contao\Model
 	 * @var string
 	 */
 	protected static $strTable = 'tl_c4g_settings';
+
+	public static function findSettings() {
+	    $db = Database::getInstance();
+	    $stmt = $db->prepare("SELECT * FROM " . self::$strTable . " LIMIT 1");
+	    return new self($stmt->execute());
+	}
 }
