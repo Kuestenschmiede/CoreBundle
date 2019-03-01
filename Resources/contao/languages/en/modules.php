86cb74d2bc82782849eda4350503d1a0c1787a3f
coastforge-cro <claudio.ross@kuestenschmiede.de>
2019-03-01 15:06:45 +0100

    Change wording

--- Resources/contao/languages/en/modules.php
+++ Resources/contao/languages/en/modules.php
@@ -15,17 +15,12 @@
 /**
  * Back end modules
  */
-/**
- * Back end modules
- */
-$GLOBALS['TL_LANG']['MOD']['con4gis'] = array('con4gis '.$GLOBALS['con4gis']['core']['version'].' - Baukasten', 'Geoinformationssysteme für Contao');
-$GLOBALS['TL_LANG']['MOD']['con4gis_bricks'] = array('con4gis '.$GLOBALS['con4gis']['core']['version'].' - Bausteine', 'con4gis Erweiterungen');
+$GLOBALS['TL_LANG']['MOD']['con4gis'] = array('con4gis '.$GLOBALS['con4gis']['core']['version'], 'Geographic information systems for Contao');
 
-$GLOBALS['TL_LANG']['MOD']['c4g_core'] = array('Info & Help', 'Information and help for con4gis.' );
+$GLOBALS['TL_LANG']['MOD']['c4g_core'] = array('Info & Help', 'Information and help for con4gis.');
 $GLOBALS['TL_LANG']['MOD']['c4g_settings'] = array('Settings', 'Default settings for all bricks.');
 
 /**
  * Content elements
  */
 $GLOBALS['TL_LANG']['CTE']['c4g_activationpage']          = array('Aktivationpage (con4gis)', 'With this module you include an activationpage.');
-?>
