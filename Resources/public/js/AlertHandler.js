aa7e37c0a272a70d69fd830ed77ed28889f8bc7f
coastforge-cro <claudio.ross@kuestenschmiede.de>
2019-06-05 12:05:21 +0200

    Add confirm dialog and select dialog functions

--- Resources/public/js/AlertHandler.js
+++ Resources/public/js/AlertHandler.js
@@ -8,14 +8,46 @@
  * @link      https://www.kuestenschmiede.de
  */
 
-import swal from 'sweetalert';
+import Swal from 'sweetalert2';
 
 export class AlertHandler {
+
   showErrorDialog(title, content) {
-    swal(title, content, "error");
+    Swal.fire(title, content, "error");
   }
 
   showInfoDialog(title, content) {
-    swal(title, content, "info");
+    Swal.fire(title, content, "info");
+  }
+
+  showConfirmDialog(title, text, confirmCallback, cancelCallback, confirmText, cancelText) {
+    Swal.fire({
+      title: title,
+      text: text,
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonText: confirmText ? confirmText : "Confirm",
+      cancelButtonText: cancelText ? cancelText : "Cancel",
+      dangerMode: true
+    }).then((willDelete) => {
+      if (willDelete.value) {
+        confirmCallback();
+      } else {
+        cancelCallback();
+      }
+    });
+  }
+
+  async showSelectDialog(title, objOptions, confirmText, cancelText) {
+    const {value: selectedValue} = await Swal.fire({
+      title: title,
+      input: 'select',
+      inputOptions: objOptions,
+      inputPlaceholder: 'Select a fruit',
+      showCancelButton: true,
+      confirmButtonText: confirmText,
+      cancelButtonText: cancelText
+    });
+    return selectedValue;
   }
 }
