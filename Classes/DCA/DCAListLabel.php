c8388a1aee4f4c15e805ab53e52acc07d5fd0efe
rro <raphael.ross@kuestenschmiede.gmbh>
2019-09-17 15:21:01 +0200

    Add labelCallback option to DCAListLabel

--- Classes/DCA/DCAListLabel.php
+++ Classes/DCA/DCAListLabel.php
@@ -16,9 +16,16 @@ class DCAListLabel
 
     public function showColumns(bool $showColumns) {
         $this->global['showColumns'] = $showColumns;
+        return $this;
     }
 
     public function fields(array $fields) {
         $this->global['fields'] = $fields;
+        return $this;
+    }
+
+    public function labelCallback(string $class, string $method) {
+        $this->global['label_callback'] = [$class, $method];
+        return $this;
     }
 }
