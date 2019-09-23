e3ba91c41dc88e7f9c65f52e5cb349d1e1881fe7
rro <raphael.ross@kuestenschmiede.gmbh>
2019-09-23 14:39:44 +0200

    Add MultiCheckboxField

---
+++ Classes/DCA/Fields/MultiCheckboxField.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace con4gis\CoreBundle\Classes\DCA\Fields;
+
+
+use con4gis\CoreBundle\Classes\DCA\DCA;
+
+class MultiCheckboxField extends DCAField
+{
+    public function __construct(string $name, DCA $dca, DCAField $multiColumnField = null) {
+        parent::__construct($name, $dca, $multiColumnField);
+        $this->inputType('checkboxWizard')
+            ->sql("text NULL")
+            ->eval()->multiple();
+    }
+}
