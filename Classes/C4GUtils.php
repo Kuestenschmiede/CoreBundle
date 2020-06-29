04b175b8c1298afe442aab78e62cb5d8e4454936
coastforge-rro <raphael.ross@kuestenschmiede.gmbh>
2020-06-29 13:38:51 +0200

    Add C4GUtils::getGUID()

--- Classes/C4GUtils.php
+++ Classes/C4GUtils.php
@@ -470,4 +470,22 @@ class C4GUtils
 
         return false;
     }
+
+    /**
+     * @return string
+     */
+    public static function getGUID()
+    {
+        mt_srand((double) microtime() * 10000);
+        $charid = strtoupper(md5(uniqid(rand(), true)));
+        $hyphen = chr(45);// "-"
+        $uuid = chr(123)// "{"
+            . substr($charid, 0, 8) . $hyphen
+            . substr($charid, 8, 4) . $hyphen
+            . substr($charid, 12, 4) . $hyphen
+            . substr($charid, 16, 4) . $hyphen
+            . substr($charid, 20, 12)
+            . chr(125);// "}"
+        return $uuid;
+    }
 }
