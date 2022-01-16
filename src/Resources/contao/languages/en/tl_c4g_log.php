b672a9d8c647785bab272830b98ff3a86d88cfd2
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2022-01-16 17:58:54 +0100

    copyright hints

--- src/Resources/contao/languages/en/tl_c4g_log.php
+++ src/Resources/contao/languages/en/tl_c4g_log.php
@@ -5,19 +5,8 @@
  * @version 8
  * @author con4gis contributors (see "authors.txt")
  * @license LGPL-3.0-or-later
- * @copyright (c) 2010-2021, by Küstenschmiede GmbH Software & Design
+ * @copyright (c) 2010-2022, by Küstenschmiede GmbH Software & Design
  * @link https://www.con4gis.org
- */
-
-/**
- * con4gis for Contao Open Source CMS
- *
- * @version   php 7
- * @package   con4gis-Core (CoreBundle)
- * @author    con4gis contributors
- * @license   GNU/LGPL http://opensource.org/licenses/lgpl-3.0.html
- * @copyright Küstenschmiede GmbH Software & Design 2011 - 2018 - 2018
- * @link      https://www.kuestenschmiede.de
  */
 
 /** FIELDS **/
