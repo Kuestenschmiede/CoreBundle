20db05f5a9b746c2ba78458d0ddb70f28c8354ef
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2022-01-30 22:09:10 +0100

    Version preps

--- src/Resources/contao/dca/tl_c4g_log.php
+++ src/Resources/contao/dca/tl_c4g_log.php
@@ -77,7 +77,7 @@ $GLOBALS['TL_DCA']['tl_c4g_log'] = array
                 'label'               => &$GLOBALS['TL_LANG']['tl_c4g_log']['delete'],
                 'href'                => 'act=delete',
                 'icon'                => 'delete.svg',
-                'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+                'attributes'          => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\'))return false;Backend.getScrollOffset()"'
             ),
             'show' => array
             (
