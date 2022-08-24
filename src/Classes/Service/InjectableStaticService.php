8adb6b34ecbf489d25f08496d7428ad5e5e57997
coastforge-rro <raphael.ross@kuestenschmiede.gmbh>
2022-08-24 09:06:06 +0200

    Add InjectableStaticService

---
+++ src/Classes/Service/InjectableStaticService.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * This file is part of con4gis, the gis-kit for Contao CMS.
+ * @package con4gis
+ * @version 8
+ * @author con4gis contributors (see "authors.txt")
+ * @license LGPL-3.0-or-later
+ * @copyright (c) 2010-2022, by Küstenschmiede GmbH Software & Design
+ * @link https://www.con4gis.org
+ */
+
+namespace con4gis\CoreBundle\Classes\Service;
+
+/**
+ * Allows dependency injection of classes used statically
+ * Usage:
+ * $injectable = new InjectableStaticService();
+ * $injectable->setClass(SomeClass::class);
+ * $injectable->someStaticMethodOfSomeClass($parameter1, $parameter2, ...);
+ */
+class InjectableStaticService
+{
+    private string $class;
+
+    public function setClass(string $class) : void
+    {
+        $this->class = $class;
+    }
+
+    public function __call($method, array $parameters)
+    {
+        return call_user_func([$this->class, $method], ...$parameters);
+    }
+}
