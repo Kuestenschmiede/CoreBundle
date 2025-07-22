90af691c30899f8c2f43faa2d428d7053ef448d9
croastforge <claudio.ross@kuestenschmiede.gmbh>
2025-07-22 14:53:43 +0200

    Skip query generation for already existing relations

--- src/Classes/C4GImport.php
+++ src/Classes/C4GImport.php
@@ -1200,6 +1200,53 @@ class C4GImport
                     $sqlStatement = '';
                     $importDataset = (array) $importDataset;
 
+                    // check for already existing relations
+                    $db = Database::getInstance();
+                    switch ($importDB) {
+                        case "tl_gutesio_data_tag_element":
+                        case "tl_gutesio_data_tag_element_values":
+                            $query = "SELECT * FROM $importDB WHERE `elementId` = ? and `tagId` = ?";
+                            $result = $db->prepare($query)->execute($importDataset['elementId'], $importDataset['tagId'])->fetchAllAssoc();
+                            if (count($result) > 0) {
+                                // skip to next loop iteration
+                                continue 2;
+                            }
+                            break;
+                        case "tl_gutesio_data_child_tag":
+                        case "tl_gutesio_data_child_tag_values":
+                            $query = "SELECT * FROM $importDB WHERE `childId` = ? and `tagId` = ?";
+                            $result = $db->prepare($query)->execute($importDataset['childId'], $importDataset['tagId'])->fetchAllAssoc();
+                            if (count($result) > 0) {
+                                // skip to next loop iteration
+                                continue 2;
+                            }
+                            break;
+                        case "tl_gutesio_data_tag_type":
+                            $query = "SELECT * FROM $importDB WHERE `typeId` = ? and `tagId` = ?";
+                            $result = $db->prepare($query)->execute($importDataset['typeId'], $importDataset['tagId'])->fetchAllAssoc();
+                            if (count($result) > 0) {
+                                // skip to next loop iteration
+                                continue 2;
+                            }
+                            break;
+                        case "tl_gutesio_data_directory_type":
+                            $query = "SELECT * FROM $importDB WHERE `typeId` = ? and `directoryId` = ?";
+                            $result = $db->prepare($query)->execute($importDataset['typeId'], $importDataset['directoryId'])->fetchAllAssoc();
+                            if (count($result) > 0) {
+                                // skip to next loop iteration
+                                continue 2;
+                            }
+                            break;
+                        case "tl_gutesio_data_element_type":
+                            $query = "SELECT * FROM $importDB WHERE `elementId` = ? and `typeId` = ?";
+                            $result = $db->prepare($query)->execute($importDataset['elementId'], $importDataset['typeId'])->fetchAllAssoc();
+                            if (count($result) > 0) {
+                                // skip to next loop iteration
+                                continue 2;
+                            }
+                            break;
+                    }
+
                     if ($queryType == 'UPDATE' && in_array('uuid', $dbFields) && $importDataset['uuid'] == '') {
                         C4gLogModel::addLogEntry('core', "Don't update dataset with id" . $importDataset['id'] . ' from table ' . $importDB . ' because of empty uuid.');
 
