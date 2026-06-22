1f2e5ccedb9d5943823616f8d5cae834cd744690
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2026-06-22 16:15:24 +0200

    add getRouteCollection result

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -8,6 +8,7 @@
  * @link https://www.con4gis.org
  */
 namespace con4gis\CoreBundle\ContaoManager;
+use Symfony\Component\Routing\RouteCollection;
 
 use con4gis\CoreBundle\con4gisCoreBundle;
 use Contao\CoreBundle\ContaoCoreBundle;
@@ -26,7 +27,7 @@ class Plugin implements RoutingPluginInterface, BundlePluginInterface
     /**
      * {@inheritdoc}
      */
-    public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel)
+    public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel): ?RouteCollection
     {
         return $resolver
             ->resolve(__DIR__.'/../Resources/config/routing.yml')
@@ -39,7 +40,7 @@ class Plugin implements RoutingPluginInterface, BundlePluginInterface
      *
      * @return ConfigInterface[]
      */
-    public function getBundles(ParserInterface $parser)
+    public function getBundles(ParserInterface $parser): array
     {
         return [
             BundleConfig::create(con4gisCoreBundle::class)
